b02211e9a025c413da588b0dc74f9398eba514fd
man-shuo <wlb1270858640@icloud.com>
2024-10-17 14:14:36 +0800

    搜图功能修复

--- run/imgSearch.py
+++ run/imgSearch.py
@@ -18,7 +18,10 @@ def main(bot, api_key, proxy, logger):
     with open('config/api.yaml', 'r', encoding='utf-8') as f:
         result = yaml.load(f.read(), Loader=yaml.FullLoader)
     cookies = result.get("e-hentai")
-    moderateK = result.get("moderate")
+    proxy=result.get("proxy")
+    sauceno_api=result.get("sauceno-api")
+    if proxy=="" or proxy==" ":
+        proxy=None
     with open('config/settings.yaml', 'r', encoding='utf-8') as f:
         result1 = yaml.load(f.read(), Loader=yaml.FullLoader)
     selfsensor = result1.get("moderate").get("selfsensor")
@@ -30,9 +33,10 @@ def main(bot, api_key, proxy, logger):
     @bot.on(GroupMessage)
     async def startYourSearch(event: GroupMessage):
         global dataGet
-        if str(event.message_chain) == "搜图":
-            await bot.send(event, "请发送要搜索的图片")
-            dataGet[event.sender.id] = []
+        if group_manage_controller(f'{event.group.id}_ing_search'):
+            if str(event.message_chain) == "搜图":
+                await bot.send(event, "请发送要搜索的图片")
+                dataGet[event.sender.id] = []
 
     @bot.on(GroupMessage)
     async def imgSearcher(event: GroupMessage):
@@ -49,170 +53,22 @@ def main(bot, api_key, proxy, logger):
                 for name, result in results.items():
                     if result and result[0]!="":
 
-            # Replace the key with your own
-            dataa = {"url": img_url, "db": "999", "api_key": api_key, "output_type": "2", "numres": "3"}
-            logger.info("发起搜图请求")
-            await bot.send(event, "正在检索....请稍候")
-            #sauceno搜图
-
-            try:
-                async with httpx.AsyncClient(proxies=proxies) as client:
-                    response = await client.post(url="https://saucenao.com/search.php", data=dataa)
-                #response = requests.post(url="https://saucenao.com/search.php", data=dataa, proxies=proxies)
-                logger.info("sauceno获取到结果" + str(response.json().get("results")[0]))
-                #logger.info("下载缩略图")
-                #filename=dict_download_img(img_url,dirc="data/pictures/imgSearchCache")
-                result = ' similarity:' + str(
-                    response.json().get("results")[0].get('header').get('similarity')) + "\n" + str(
-                    response.json().get("results")[0].get('data')).replace(",", "\n").replace("{", " ").replace("}",
-                                                                                                                "").replace(
-                    "'", "").replace("[", "").replace("]", "")
-                urlss = str(response.json().get("results")[0].get('header').get('thumbnail'))
-                #聊天记录模式不再可用，因此关闭
-
-                if selfsensor:
-                    try:
-                        thurs = await setuModerate(urlss, moderateK)
-                        logger.info(f"获取到审核结果： adult- {thurs}")
-                        if int(thurs) > selfthreshold:
-                            logger.warning(f"不安全的图片，自我审核过滤")
-                            urlss = "data/colorfulAnimeCharacter/" + random.choice(
-                                os.listdir("data/colorfulAnimeCharacter"))
-                    except Exception as e:
-                        logger.error(e)
-                        logger.error("无法进行自我审核，错误的网络环境或apikey不可用")
-                '''b1 = ForwardMessageNode(sender_id=bot.qq, sender_name="Manyana",
-                                        message_chain=MessageChain(["sauceno获取到结果:\n" + result, Image(url=urlss)]))'''
-                b1 = ForwardMessageNode(sender_id=bot.qq, sender_name="Manyana",
-                                        message_chain=MessageChain(["sauceno获取到结果：\n" + result]))
-                # b1 = ForwardMessageNode(sender_id=bot.qq, sender_name="Manyana",message_chain=MessageChain([result, Image(url=urlss]))
-                dataGet.get(event.sender.id).append(b1)
-
-                #await bot.send(event,' similarity:'+str(response.json().get("results")[0].get('header').get('similarity'))+"\n"+str(response.json().get("results")[0].get('data')).replace(",","\n").replace("{"," ").replace("}","").replace("'","").replace("[","").replace("]",""),True)
-            except:
-                logger.error("sauceno搜图失败，无结果或访问次数过多，请稍后再试")
-                #b1 = ForwardMessageNode(sender_id=bot.qq, sender_name="Manyana",
-                #message_chain=MessageChain(["sauceno搜图失败，无结果或访问次数过多，请稍后再试",Image(path="data/autoReply/imageReply/axaAaRaUaaafa7a.png")]))
-                #聊天记录不再可用，暂时关闭
-                #dataGet.get(event.sender.id).append(b1)
-
-            #使用TraceMoe搜图
-            try:
-                result, piccc = await test(url=img_url, proxies=proxy)
-                logger.info("TraceMoe获取到结果：" + result)
-
-                if selfsensor:
-                    try:
-                        thurs = await setuModerate(piccc, moderateK)
-                        logger.info(f"获取到审核结果： adult- {thurs}")
-                        if int(thurs) > selfthreshold:
-                            logger.warning(f"不安全的图片，自我审核过滤")
-                            piccc = "data/colorfulAnimeCharacter/" + random.choice(
-                                os.listdir("data/colorfulAnimeCharacter"))
-                    except Exception as e:
-                        logger.error(e)
-                        logger.error("无法进行自我审核，错误的网络环境或apikey不可用")
-                '''b1 = ForwardMessageNode(sender_id=bot.qq, sender_name="Manyana",
-                                        message_chain=MessageChain(
-                                            ["TraceMoe获取到结果：\n" + result, Image(url=piccc)]))'''
-                b1 = ForwardMessageNode(sender_id=bot.qq, sender_name="Manyana",
-                                        message_chain=MessageChain(
-                                            ["TraceMoe获取到结果：\n" + result]))
-                dataGet.get(event.sender.id).append(b1)
-            except:
-                logger.error("TraceMoe未获取到结果")
-                #b1 = ForwardMessageNode(sender_id=bot.qq, sender_name="Manyana",
-                #message_chain=MessageChain(["TraceMoe搜图失败，无结果或访问次数过多，请稍后再试", Image(
-                #path="data/autoReply/imageReply/axaAaRaUaaafa7a.png")]))
-                # b1 = ForwardMessageNode(sender_id=bot.qq, sender_name="Manyana",message_chain=MessageChain([result, Image(url=urlss]))
-                #dataGet.get(event.sender.id).append(b1)
-            #使用Ascii2D
-            try:
-                result, piccc = await test1(url=img_url, proxies=proxy)
-                logger.info("Ascii2D获取到结果：\n" + result)
-
-                if selfsensor:
-                    try:
-                        thurs = await setuModerate(piccc, moderateK)
-                        logger.info(f"获取到审核结果： adult- {thurs}")
-                        if int(thurs) > selfthreshold:
-                            logger.warning(f"不安全的图片，自我审核过滤")
-                            piccc = "data/colorfulAnimeCharacter/" + random.choice(
-                                os.listdir("data/colorfulAnimeCharacter"))
-                    except Exception as e:
-                        logger.error(e)
-                        logger.error("无法进行自我审核，错误的网络环境或apikey不可用")
-                '''b1 = ForwardMessageNode(sender_id=bot.qq, sender_name="Manyana",
-                 message_chain=MessageChain(["Ascii2D获取到结果：\n" +result, Image(url=piccc)]))'''
-                b1 = ForwardMessageNode(sender_id=bot.qq, sender_name="Manyana",
-                                        message_chain=MessageChain(["Ascii2D获取到结果：\n" + result]))
-                dataGet.get(event.sender.id).append(b1)
-            except:
-                logger.error("Ascii2D未获取到结果")
-                b1 = ForwardMessageNode(sender_id=bot.qq, sender_name="Manyana",
-                                        message_chain=MessageChain(
-                                            ["Ascii2D搜图失败，无结果或访问次数过多，请稍后再试", Image(
-                                                path="data/autoReply/imageReply/axaAaRaUaaafa7a.png")]))
-
-                dataGet.get(event.sender.id).append(b1)
-
-            # 使用IQDB
-            try:
-                result, piccc = await superSearch(url=img_url, proxies=proxy)
-                logger.info("iqdb获取到结果：\n" + result)
-
-                if selfsensor:
-                    try:
-                        thurs = await setuModerate(piccc, moderateK)
-                        logger.info(f"获取到审核结果： adult- {thurs}")
-                        if int(thurs) > selfthreshold:
-                            logger.warning(f"不安全的图片，自我审核过滤")
-                            piccc = "data/colorfulAnimeCharacter/" + random.choice(
-                                os.listdir("data/colorfulAnimeCharacter"))
-                    except Exception as e:
-                        logger.error(e)
-                        logger.error("无法进行自我审核，错误的网络环境或apikey不可用")
-                '''b1 = ForwardMessageNode(sender_id=bot.qq, sender_name="Manyana",
-                                        message_chain=MessageChain(["iqdb获取到结果：\n" + result, Image(url=piccc)]))'''
-                b1 = ForwardMessageNode(sender_id=bot.qq, sender_name="Manyana",
-                                        message_chain=MessageChain(["iqdb获取到结果：\n" + result]))
-                dataGet.get(event.sender.id).append(b1)
-
-            except:
-                logger.error("iqdb未获取到结果")
-                '''try:
-                    b1 = ForwardMessageNode(sender_id=bot.qq, sender_name="Manyana",
-                                            message_chain=MessageChain([result, Image(
-                                                path="data/autoReply/imageReply/axaAaRaUaaafa7a.png")]))
-                except:
-                    b1 = ForwardMessageNode(sender_id=bot.qq, sender_name="Manyana",
-                                            message_chain=MessageChain(["iqdb搜图失败，无结果或访问次数过多，请稍后再试", Image(
-                                                path="data/autoReply/imageReply/axaAaRaUaaafa7a.png")]))
-                # b1 = ForwardMessageNode(sender_id=bot.qq, sender_name="Manyana",message_chain=MessageChain([result, Image(url=urlss]))
-                #dataGet.get(event.sender.id).append(b1)'''
-            # 使用E-hentai
-            '''try:
-                result, piccc = await test2(url=img_url, proxies=proxy,cookies=cookies)
-                logger.info("E-hentai获取到结果：" + result)
-                b1 = ForwardMessageNode(sender_id=bot.qq, sender_name="Manyana",
-                                        message_chain=MessageChain(["E-hentai获取到结果：\n" +result, Image(url=piccc)]))
-                dataGet.get(event.sender.id).append(b1)
-                try:
-                    pass
-                    #await bot.send(event, (result, Image(url=piccc)))
-                except:
-                    await bot.send(event, result)
-            except:
-                logger.error("E-hentai未获取到结果")
-                b1 = ForwardMessageNode(sender_id=bot.qq, sender_name="Manyana",
-                                        message_chain=MessageChain(["E-hentai搜图失败，无结果或访问次数过多，请稍后再试", Image(
-                                            path="data/autoReply/imageReply/axaAaRaUaaafa7a.png")]))
-                # b1 = ForwardMessageNode(sender_id=bot.qq, sender_name="Manyana",message_chain=MessageChain([result, Image(url=urlss]))
-                dataGet.get(event.sender.id).append(b1)'''
-            try:
-                await bot.send(event, Forward(node_list=dataGet.get(event.sender.id)))
-
-            except Exception as e:
-                logger.error(e)
-                await bot.send(event, "出错，请稍后再试")
-            dataGet.pop(event.sender.id)
+                        logger.info(f"{name} 成功返回: {result}")
+                        try:
+                            path="data/pictures/cache/" + random_str() + ".png"
+                            imgpath=await picDwn(result[0],path,proxy)
+                            b1 = ForwardMessageNode(sender_id=bot.qq, sender_name="Manyana",
+                                                    message_chain=MessageChain([ Image(path=imgpath),result[1] ]))
+                        except Exception as e:
+                            logger.error(f"预览图{name} 下载失败: {e}")
+                            b1 = ForwardMessageNode(sender_id=bot.qq, sender_name="Manyana",message_chain=MessageChain([ Image(url=result[0]),result[1] ]))
+                        if name=="ascii2d_async":
+                            b1 = ForwardMessageNode(sender_id=bot.qq, sender_name="Manyana",
+                                                    message_chain=MessageChain([Image(url=img_url), result[1]]))
+                        forMeslist.append(b1)
+                    else:
+                        logger.error(f"{name} 返回失败或无结果")
+                        b1 = ForwardMessageNode(sender_id=bot.qq, sender_name="Manyana",
+                                                message_chain=MessageChain([f"{name} 返回失败或无结果"]))
+                        forMeslist.append(b1)
+                await bot.send(event, Forward(node_list=forMeslist))
